0baf6ef851c5565ba75fb9841298eaa1d4389e7e
FahmiAchmadF <chiromatsu@gmail.com>
2018-02-08 11:45:33 +0700

    - add avatar, username, name for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207072036) do
+ActiveRecord::Schema.define(version: 20180208035200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(version: 20180207072036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "roles", default: ["user"], array: true
+    t.string "avatar"
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
